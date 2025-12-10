2acd9d0204cb47152824f8249b8142c4b151b011
Женя <147931012+Zhenya22886@users.noreply.github.com>
2025-12-10 18:15:16 +0200

    Set up PostgreSQL, SQLAlchemy and Migrate for Lab 3

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+migrate = Migrate()
 
 app = Flask(__name__)
 
-from app import views
+app.config.from_pyfile("config.py", silent=False)
+
+db.init_app(app)
+migrate.init_app(app, db)
+
+from app import views 
